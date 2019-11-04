ba68a6589d0fa08593d1b0f0916910cc9c83123a
MinexAutomation <minex.automation@gmail.com>
2019-11-04 11:54:07 -0800

    Added web host builder extensions.

---
+++ source/R5T.Herulia/Code/Extensions/IWebHostBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace R5T.Herulia.Extensions
+{
+    public static class IWebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UseDefaultContentRoot(this IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+            return webHostBuilder;
+        }
+    }
+}
