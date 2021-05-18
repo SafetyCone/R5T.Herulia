aa0e51311e503c9e0b4860bf0fee688f182e6a1b
David Coats <david@getsafetycone.com>
2021-05-17 22:10:24 -0700

    Add R5T.Herulia.Extensions library.

---
+++ source/R5T.Herulia.Extensions/Code/Helpers/WebHostBuilderHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using R5T.Herulia.Extensions;
+
+
+namespace Microsoft.AspNetCore.Hosting
+{
+    public static class WebHostBuilderHelper
+    {
+        /// <summary>
+        /// Default includes:
+        /// * UseKestrel().
+        /// * UseDefaultContentRoot() (current directory as the default content root).
+        /// * UseIISIntegration().
+        /// </summary>
+        public static IWebHostBuilder GetDefaultWebHostBuilder()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseKestrel()
+                .UseDefaultContentRoot()
+                .UseIISIntegration()
+                ;
+
+            return webHostBuilder;
+        }
+    }
+}
